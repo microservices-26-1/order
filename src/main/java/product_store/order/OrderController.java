834292e7030d96e60082e6088706ad4b6d700d24
Ana Beatriz da Cunha <133781643+aninhaabc@users.noreply.github.com>
2026-05-20 12:17:11 -0300

    fix order controller

--- src/main/java/product_store/order/OrderController.java
+++ src/main/java/product_store/order/OrderController.java
@@ -9,28 +9,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
-    name="order",
-    url="http://order:8080"
+    name = "order",
+    url = "http://order:8080"
 )
 public interface OrderController {
 
     @PostMapping("/orders")
-    public ResponseEntity<Void> create(
+    ResponseEntity<OrderOut> create(
         @RequestBody OrderIn in,
-        @RequestHeader(value = "id-account", required = true) String idAccount
+        @RequestHeader("id-account") String idAccount
     );
 
     @GetMapping("/orders")
-    public ResponseEntity<List<OrderOut>> healthCheck();
-
-    @GetMapping("/orders")
-    public ResponseEntity<List<OrderOut>> findAll();
+    ResponseEntity<List<OrderOut>> findAll(
+        @RequestHeader("id-account") String idAccount
+    );
 
     @GetMapping("/orders/{id}")
-    public ResponseEntity<List<OrderOut>> findById(
-        @PathVariable String id
+    ResponseEntity<OrderOut> findById(
+        @PathVariable String id,
+        @RequestHeader("id-account") String idAccount,
+        @RequestParam(required = false) String currency
     );
-
 }
